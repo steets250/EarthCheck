d1186f64ebc6805b28bf09e25cff335be50e2127
Justin Nguyen <justnguyen1@gmail.com>
2020-04-25 22:08:21 -0700

    progess bar

--- pages/DashboardPage/index.js
+++ pages/DashboardPage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
-import { Button, Flex } from '@ant-design/react-native';
+import { Button, Flex, Progress } from '@ant-design/react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 const profile = require("../../assets/justin.jpg");
@@ -14,7 +14,11 @@ const DashboardPage = () => {
                     <Flex direction="row" justify="center" align="center" style={{marginTop: 60}}>
                         <Image source={profile} style={{width: 100, height: 100, borderRadius: 50, }}></Image>
                     </Flex>
-                    
+                    <Text style={{ color: "white", textAlign: "center", fontSize: 16, fontFamily: 'Avenir-Light', fontWeight: "bold", marginTop: 15}}>Justin Nguyen</Text>
+                    <Text style={{ color: "white", textAlign: "center", fontSize: 16, fontFamily: 'Avenir-Light',  marginTop: 10}}>Level 10</Text>
+                    <Text style={{ color: "white", textAlign: "center", fontSize: 16, fontFamily: 'Avenir-Light',  }}>Solider of Gaia</Text>
+                    <Progress percent={80} style={{ marginHorizontal: 120, marginTop: 20, }}></Progress>
+
                 </View>
             </SafeAreaView>
         </SafeAreaProvider>
