b8f9d2c46c48248520b317063e4635e06f39390a
Justin Nguyen <justnguyen1@gmail.com>
2020-04-25 21:40:43 -0700

    dashboard and login page

--- pages/DashboardPage/index.js
+++ pages/DashboardPage/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
-import { Button } from '@ant-design/react-native';
+import { Button, Flex } from '@ant-design/react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
+const profile = require("../../assets/justin.jpg");
 
 const DashboardPage = () => {
     return (
         <SafeAreaProvider>
-            <SafeAreaView>
+            <SafeAreaView style={{backgroundColor: "#5FBA62", height: "50%"}}>
                 <View>
-                    <Text style={{ textAlign: "left", fontSize: 34, fontFamily: 'Avenir-Light', marginLeft: 20 }}>Dashboard</Text>
-                    
+                    <Text style={{ color: "white", textAlign: "left", fontSize: 34, fontFamily: 'Avenir-Light', marginLeft: 20 }}>Dashboard</Text>
+                    <Flex direction="row" justify="center" align="center" style={{marginTop: 60}}>
+                        <Image source={profile} style={{width: 100, height: 100, borderRadius: 50, }}></Image>
+                    </Flex>
                     
                 </View>
             </SafeAreaView>
--- pages/LoginPage/index.js
+++ pages/LoginPage/index.js
@@ -9,7 +9,7 @@ const LoginPage = () => {
         <View style={{ backgroundColor: "#E5E5E5", height: "100%" }}>
             <ImageBackground source={background} style={{width: '100%', height: '100%'}}>
                 <Text style={{ textAlignVertical: "center", textAlign: "center", marginTop: 350, marginBottom: 350, fontSize: 45, fontFamily: 'Avenir-Light' }}>EarthCheck</Text>
-                <Button type="primary" style={{ backgroundColor: "#02D40A", borderRadius: 20, marginHorizontal: 80, height: 60}}>login</Button>
+                <Button type="primary" style={{ backgroundColor: "#5FBA62", borderRadius: 20, marginHorizontal: 80, height: 60}}>login</Button>
             </ImageBackground>
         </View>
     );
