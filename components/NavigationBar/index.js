8e1c0aafd87ad43efb002803f5b4c51d22c68f1a
Justin Nguyen <justnguyen1@gmail.com>
2020-04-26 05:30:56 -0700

    Update index.js

--- components/NavigationBar/index.js
+++ components/NavigationBar/index.js
@@ -1,30 +1,41 @@
 import React from "react";
-
-import { View, Text } from "react-native";
+import { Flex } from '@ant-design/react-native';
+import { View, Text, Image } from "react-native";
 import { Link } from "react-router-native";
+
+const dashboard = require("../../assets/icons/dashboard.png");
+const checklist = require("../../assets/icons/checklist.png");
+const leaderboard = require("../../assets/icons/leaderboard.png");
 
 const NavigationBar = () => {
     return (
-        <View>
-        <Link
-          to='/dashboard'
-          underlayColor="#f0f4f7"
-        >
-          <Text>Rendering with React</Text>
-        </Link>
-        <Link
-          to='/checklist'
-          underlayColor="#f0f4f7"
-        >
-          <Text>Components</Text>
-        </Link>
-        <Link
-          to='/leaderboard'
-          underlayColor="#f0f4f7"
-        >
-          <Text>Props v. State</Text>
-        </Link>
-      </View>
+      <Flex direction="row">
+        <View style={{marginLeft: 55, marginRight: 45, marginTop: 15}}>
+          <Link
+            to='/dashboard'
+            underlayColor="#f0f4f7"
+          >
+            <Image source={dashboard} style={{ width: 40, height: 40,}}></Image>
+          </Link>
+        </View>
+        <View style={{marginLeft: 45, marginRight: 45, marginTop: 15}}>
+          <Link
+            to='/checklist'
+            underlayColor="#f0f4f7"
+          >
+            <Image source={checklist} style={{ width: 40, height: 40,}}></Image>
+          </Link>
+        </View>
+        <View style={{marginLeft: 45, marginTop: 15}}>
+          <Link
+            to='/leaderboard'
+            underlayColor="#f0f4f7"
+          >
+        
+            <Image source={leaderboard} style={{ width: 40, height: 40,}}></Image>
+          </Link>
+        </View>
+      </Flex>
     );
   }
 export default NavigationBar;
